ba91bdd5ce2f134d647b81d53c3c2878b38b81d9
Brandon Iles <brandon@frgcoin.com>
2018-07-16 17:22:27 -0700

    test cleanup

--- test/unit/UFragmentsPolicy.js
+++ test/unit/UFragmentsPolicy.js
@@ -21,75 +21,75 @@ contract('uFragmentsPolicy', async accounts => {
     await proxy.storeSupply(uFragSupply);
   }
 
-  describe('Rebase', () => {
+  describe('Rebase', function () {
     let r, _epoch, _time;
 
-    describe('when minRebaseTimeIntervalSec has NOT passed since the previous rebase', () => {
-      before(async () => {
+    describe('when minRebaseTimeIntervalSec has NOT passed since the previous rebase', function () {
+      before(async function () {
         snapshot = await chain.snapshotChain();
         await mockExternalData(1.3e18, 100, 1010);
         await uFragmentsPolicy.rebase();
       });
-      after(async () => {
+      after(async function () {
         await chain.revertToSnapshot(snapshot);
       });
 
-      it('should fail', async () => {
+      it('should fail', async function () {
         await chain.expectEthException(uFragmentsPolicy.rebase());
       });
     });
 
-    describe('when rate is withinDeviationThreshold', () => {
-      before(async () => {
+    describe('when rate is withinDeviationThreshold', function () {
+      before(async function () {
         snapshot = await chain.snapshotChain();
         await mockExternalData(1.0499e18, 100, 1000);
       });
-      after(async () => {
+      after(async function () {
         await chain.revertToSnapshot(snapshot);
       });
 
-      it('should return 0', async () => {
+      it('should return 0', async function () {
         r = await uFragmentsPolicy.rebase();
         expect(r.logs[0].args.appliedSupplyAdjustment.toNumber()).to.eq(0);
       });
     });
 
-    describe('when uFragments grows beyond MAX_SUPPLY', () => {
-      before(async () => {
+    describe('when uFragments grows beyond MAX_SUPPLY', function () {
+      before(async function () {
         snapshot = await chain.snapshotChain();
         await mockExternalData(2e18, 100, new BigNumber(2).pow(128).minus(2));
       });
-      after(async () => {
+      after(async function () {
         await chain.revertToSnapshot(snapshot);
       });
 
       // Supply is {2^128-2}, exchangeRate is 2x; resulting in a new supply more than MAX_SUPPLY={2^128-1}
       // Supply is increased by 1 to MAX_SUPPLY
-      it('should apply SupplyAdjustment {MAX_SUPPLY - totalSupply}', async () => {
+      it('should apply SupplyAdjustment {MAX_SUPPLY - totalSupply}', async function () {
         r = await uFragmentsPolicy.rebase();
         expect(r.logs[0].args.appliedSupplyAdjustment.toNumber()).to.eq(1);
       });
     });
 
-    describe('when uFragments supply equals MAX_SUPPLY', () => {
-      before(async () => {
+    describe('when uFragments supply equals MAX_SUPPLY', function () {
+      before(async function () {
         snapshot = await chain.snapshotChain();
         await mockExternalData(2e18, 100, new BigNumber(2).pow(128).minus(1));
       });
-      after(async () => {
+      after(async function () {
         await chain.revertToSnapshot(snapshot);
       });
 
-      it('should apply SupplyAdjustment=0', async () => {
+      it('should apply SupplyAdjustment=0', async function () {
         r = await uFragmentsPolicy.rebase();
         expect(r.logs[0].args.appliedSupplyAdjustment.toNumber()).to.eq(0);
       });
     });
 
-    describe('when minRebaseTimeIntervalSec has passed since the previous rebase', () => {
-      describe('positive rate', () => {
+    describe('when minRebaseTimeIntervalSec has passed since the previous rebase', function () {
+      describe('positive rate', function () {
         let uFragSpy;
-        before(async () => {
+        before(async function () {
           snapshot = await chain.snapshotChain();
           await mockExternalData(1.3e18, 100, 1000);
           await uFragmentsPolicy.rebase();
@@ -101,33 +101,33 @@ contract('uFragmentsPolicy', async accounts => {
           uFragSpy.watch();
           r = await uFragmentsPolicy.rebase();
         });
-        after(async () => {
+        after(async function () {
           uFragSpy.stopWatching();
           await chain.revertToSnapshot(snapshot);
         });
 
-        it('should increment epoch', async () => {
+        it('should increment epoch', async function () {
           const epoch = await uFragmentsPolicy.epoch.call();
           expect(_epoch.plus(1).eq(epoch));
         });
-        it('should update lastRebaseTimestamp', async () => {
+        it('should update lastRebaseTimestamp', async function () {
           const time = await uFragmentsPolicy.lastRebaseTimestamp.call();
           expect(time.minus(_time).gte(3600 * 24)).to.be.true;
         });
-        it('should emit Rebase with positive appliedSupplyAdjustment', async () => {
+        it('should emit Rebase with positive appliedSupplyAdjustment', async function () {
           const log = r.logs[0];
           expect(log.event).to.eq('Rebase');
           expect(log.args.epoch.eq(_epoch.plus(1))).to.be.true;
           expect(log.args.appliedSupplyAdjustment.toNumber()).to.eq(20);
           expect(log.args.volume.toNumber()).to.eq(100);
         });
-        it('should call getPriceAndVolume from the market oracle', async () => {
+        it('should call getPriceAndVolume from the market oracle', async function () {
           const fnCalls = new ProxyContractFunctionSpy(uFragSpy).getCalledFunctions();
           expect(fnCalls[0].fnName).to.eq('MarketOracle:getPriceAndVolume');
           expect(fnCalls[0].calledBy).to.eq(uFragmentsPolicy.address);
           expect(fnCalls[0].arguments).to.be.empty;
         });
-        it('should call uFrag Rebase', async () => {
+        it('should call uFrag Rebase', async function () {
           const fnCalls = new ProxyContractFunctionSpy(uFragSpy).getCalledFunctions();
           const epoch = await uFragmentsPolicy.epoch.call();
           expect(fnCalls[1].fnName).to.eq('UFragments:rebase');
@@ -136,21 +136,21 @@ contract('uFragmentsPolicy', async accounts => {
         });
       });
 
-      describe('negative rate', () => {
+      describe('negative rate', function () {
         let uFragSpy;
-        before(async () => {
+        before(async function () {
           snapshot = await chain.snapshotChain();
           uFragSpy = new ContractEventSpy([proxy.FunctionCalled, proxy.FunctionArguments]);
           uFragSpy.watch();
           await mockExternalData(0.7e18, 100, 1000);
           r = await uFragmentsPolicy.rebase();
         });
-        after(async () => {
+        after(async function () {
           uFragSpy.stopWatching();
           await chain.revertToSnapshot(snapshot);
         });
 
-        it('should emit Rebase with negative appliedSupplyAdjustment', async () => {
+        it('should emit Rebase with negative appliedSupplyAdjustment', async function () {
           const log = r.logs[0];
           expect(log.event).to.eq('Rebase');
           expect(log.args.appliedSupplyAdjustment.toNumber()).to.eq(-10);
--- test/unit/erc20_behavior.js
+++ test/unit/erc20_behavior.js
@@ -3,15 +3,15 @@ const uFragments = artifacts.require('UFragments.sol');
 const _require = require('app-root-path').require;
 const { StandardTokenBehavior } = _require('/test/behavior/erc20');
 
-contract('uFragments', async accounts => {
+contract('uFragments', function (accounts) {
   let mFragments;
 
-  before(async function () {
+  beforeEach('setup contract for each test', async function () {
     mFragments = await uFragments.deployed();
   });
 
-  describe('uFragments as ERC20', () => {
-    it('should confirm to the specification', () => {
+  describe('uFragments as ERC20', function () {
+    it('should confirm to the specification', function () {
       const owner = accounts[0];
       const anotherAccount = accounts[8];
       const recipient = accounts[9];
