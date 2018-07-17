cb43bedbe7a01076168122d1caf2c3b4d3c1a5d9
Brandon Iles <brandon.iles@gmail.com>
2018-07-17 10:49:23 -0700

    Remove arrow functions in Mocha tests (#12)

--- test/unit/uFragments.js
+++ test/unit/uFragments.js
@@ -5,7 +5,7 @@ const { ContractEventSpy } = _require('/util/spies');
 const BlockchainCaller = _require('/util/blockchain_caller');
 const chain = new BlockchainCaller(web3);
 
-contract('UFragments', async accounts => {
+contract('UFragments', function (accounts) {
   let uFragments, snapshot, b;
   const deployer = accounts[0];
   const policy = accounts[1];
@@ -13,40 +13,40 @@ contract('UFragments', async accounts => {
   const B = accounts[3];
   const C = accounts[4];
 
-  before(async function () {
-    uFragments = await UFragments.deployed();
+  before('setup contract for each test', async function () {
+    uFragments = await UFragments.new();
     await uFragments.setMonetaryPolicy(policy, {from: deployer});
   });
 
-  describe('on initialization', () => {
-    it('should add +1000 uFragments to the deployer', async () => {
+  describe('on initialization', function () {
+    it('should add +1000 uFragments to the deployer', async function () {
       b = await uFragments.balanceOf.call(deployer);
       expect(b.toNumber()).to.eq(1000);
     });
-    it('should set the totalSupply to 1000', async () => {
+    it('should set the totalSupply to 1000', async function () {
       b = await uFragments.totalSupply.call();
       expect(b.toNumber()).to.eq(1000);
     });
   });
 
-  describe('Monetary Policy', () => {
-    it('should not be set-able by non-owner', async () => {
+  describe('Monetary Policy', function () {
+    it('should not be set-able by non-owner', async function () {
       await chain.expectEthException(
         uFragments.setMonetaryPolicy(A, { from: policy })
       );
     });
   });
 
-  describe('Transfer', () => {
-    before(async () => {
+  describe('Transfer', function () {
+    before(async function () {
       snapshot = await chain.snapshotChain();
     });
-    after(async () => {
+    after(async function () {
       await chain.revertToSnapshot(snapshot);
     });
 
-    describe('deployer transfers 12 to A', () => {
-      it('should have balances [988,12]', async () => {
+    describe('deployer transfers 12 to A', function () {
+      it('should have balances [988,12]', async function () {
         await uFragments.transfer(A, 12, { from: deployer });
         b = await uFragments.balanceOf.call(deployer);
         expect(b.toNumber()).to.eq(988);
@@ -55,8 +55,8 @@ contract('UFragments', async accounts => {
       });
     });
 
-    describe('deployer transfers 15 to B', async () => {
-      it('should have balances [973,15]', async () => {
+    describe('deployer transfers 15 to B', async function () {
+      it('should have balances [973,15]', async function () {
         await uFragments.transfer(B, 15, { from: deployer });
         b = await uFragments.balanceOf.call(deployer);
         expect(b.toNumber()).to.eq(973);
@@ -65,9 +65,9 @@ contract('UFragments', async accounts => {
       });
     });
 
-    describe('deployer transfers the rest to C', async () => {
+    describe('deployer transfers the rest to C', async function () {
       //      it('should have balances [0, 973]');
-      it('should have balances [0,973]', async () => {
+      it('should have balances [0,973]', async function () {
         await uFragments.transfer(C, 973, { from: deployer });
         b = await uFragments.balanceOf.call(deployer);
         expect(b.toNumber()).to.eq(0);
@@ -77,26 +77,26 @@ contract('UFragments', async accounts => {
     });
   });
 
-  describe('Rebase', async () => {
+  describe('Rebase', async function () {
     describe('Access Controls', function () {
       let rebaseSpy;
-      before(async () => {
+      before(async function () {
         snapshot = await chain.snapshotChain();
         await uFragments.transfer(A, 250, { from: deployer });
         rebaseSpy = new ContractEventSpy([uFragments.Rebase]);
         rebaseSpy.watch();
         await uFragments.rebase(1, 500, {from: policy});
       });
-      after(async () => {
+      after(async function () {
         rebaseSpy.stopWatching();
         await chain.revertToSnapshot(snapshot);
       });
 
-      it('should be callable by monetary policy', async () => {
+      it('should be callable by monetary policy', async function () {
         await uFragments.rebase(1, 10, {from: policy});
       });
 
-      it('should not be callable by others', async () => {
+      it('should not be callable by others', async function () {
         await chain.expectEthException(
           uFragments.rebase(1, 500, { from: deployer })
         );
@@ -106,30 +106,30 @@ contract('UFragments', async accounts => {
     describe('Expansion', function () {
       let rebaseSpy;
       // Rebase +500 (50%), with starting balances deployer:750 and A:250.
-      before(async () => {
+      before(async function () {
         snapshot = await chain.snapshotChain();
         await uFragments.transfer(A, 250, { from: deployer });
         rebaseSpy = new ContractEventSpy([uFragments.Rebase]);
         rebaseSpy.watch();
         await uFragments.rebase(1, 500, {from: policy});
       });
-      after(async () => {
+      after(async function () {
         rebaseSpy.stopWatching();
         await chain.revertToSnapshot(snapshot);
       });
 
-      it('should increase the totalSupply', async () => {
+      it('should increase the totalSupply', async function () {
         b = await uFragments.totalSupply.call();
         expect(b.toNumber()).to.eq(1500);
       });
-      it('should increase individual balances', async () => {
+      it('should increase individual balances', async function () {
         b = await uFragments.balanceOf.call(deployer);
         expect(b.toNumber()).to.be.above(750).and.at.most(1125);
 
         b = await uFragments.balanceOf.call(A);
         expect(b.toNumber()).to.be.above(250).and.at.most(375);
       });
-      it('should emit Rebase', async () => {
+      it('should emit Rebase', async function () {
         const rebaseEvent = rebaseSpy.getEventByName('Rebase');
         expect(rebaseEvent).to.exist;
         expect(rebaseEvent.args.epoch.toNumber()).to.eq(1);
@@ -140,30 +140,29 @@ contract('UFragments', async accounts => {
     describe('Contraction', function () {
       let rebaseSpy;
       // Rebase -500 (-50%), with starting balances deployer:750 and A:250.
-      before(async () => {
+      before(async function () {
         snapshot = await chain.snapshotChain();
         await uFragments.transfer(A, 250, { from: deployer });
         rebaseSpy = new ContractEventSpy([uFragments.Rebase]);
         rebaseSpy.watch();
         await uFragments.rebase(1, -500, {from: policy});
       });
-      after(async () => {
-        rebaseSpy.stopWatching();
+      after(async function () {
         await chain.revertToSnapshot(snapshot);
       });
 
-      it('should decrease the totalSupply', async () => {
+      it('should decrease the totalSupply', async function () {
         b = await uFragments.totalSupply.call();
         expect(b.toNumber()).to.eq(500);
       });
-      it('should decrease individual balances', async () => {
+      it('should decrease individual balances', async function () {
         b = await uFragments.balanceOf.call(deployer);
         expect(b.toNumber()).to.at.least(374).and.at.most(375);
 
         b = await uFragments.balanceOf.call(A);
         expect(b.toNumber()).to.at.least(124).and.at.most(125);
       });
-      it('should emit Rebase', async () => {
+      it('should emit Rebase', async function () {
         const rebaseEvent = rebaseSpy.getEventByName('Rebase');
         expect(rebaseEvent).to.exist;
         expect(rebaseEvent.args.epoch.toNumber()).to.eq(1);
