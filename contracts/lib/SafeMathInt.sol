05d7e271ed29d3e42b032861b14e4c48be8c1c6e
Brandon Iles <brandon@fragments.org>
2018-09-25 15:07:44 -0700

    changes 1

--- contracts/lib/SafeMathInt.sol
+++ contracts/lib/SafeMathInt.sol
@@ -1,10 +1,34 @@
+/*
+MIT License
+
+Copyright (c) 2018 requestnetwork
+Copyright (c) 2018 Fragments, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 pragma solidity 0.4.24;
 
 
 /**
  * @title SafeMathInt
- * @dev Math operations for int256 with overflow safety checks that results
- *      in failure if violated.
+ * @dev Math operations for int256 with overflow safety checks.
  */
 library SafeMathInt {
     int256 private constant MIN_INT256 = int256(1) << 255;
@@ -21,8 +45,7 @@ library SafeMathInt {
         int256 c = a * b;
 
         // Detect overflow when multiplying MIN_INT256 with -1
-        require(c != MIN_INT256 ||
-            (a & MIN_INT256) != (b & MIN_INT256));
+        require(c != MIN_INT256 || (a & MIN_INT256) != (b & MIN_INT256));
         require((b == 0) || (c / b == a));
         return c;
     }
@@ -38,10 +61,8 @@ library SafeMathInt {
         // Prevent overflow when dividing MIN_INT256 by -1
         require(b != -1 || a != MIN_INT256);
 
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        int256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
+        // Solidity already throws when dividing by 0.
+        return a / b;
     }
 
     /**
@@ -52,9 +73,9 @@ library SafeMathInt {
         pure
         returns (int256)
     {
-        require((b >= 0 && a - b <= a) || (b < 0 && a - b > a));
-
-        return a - b;
+        int256 c = a - b;
+        require((b >= 0 && c <= a) || (b < 0 && c > a));
+        return c;
     }
 
     /**
