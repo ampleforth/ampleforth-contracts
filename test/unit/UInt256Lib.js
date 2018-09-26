b6a7ff18b08d868a3991b86f5bd1acf8f1139916
Brandon Iles <brandon@fragments.org>
2018-09-26 11:39:20 -0700

    rename test file and mock

--- test/unit/UInt256Lib.js
+++ test/unit/UInt256Lib.js
@@ -1,4 +1,4 @@
-const SafeMathUIntMock = artifacts.require('SafeMathUIntMock');
+const UInt256LibMock = artifacts.require('UInt256LibMock');
 
 const BigNumber = web3.BigNumber;
 const _require = require('app-root-path').require;
@@ -9,13 +9,13 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-contract('SafeMathUInt', () => {
+contract('UInt256Lib', () => {
   const MAX_INT256 = new BigNumber(2).pow(255).minus(1);
 
-  let safeMathUInt;
+  let UInt256Lib;
 
   beforeEach(async function () {
-    safeMathUInt = await SafeMathUIntMock.new();
+    UInt256Lib = await UInt256LibMock.new();
   });
 
   async function returnVal (tx) {
@@ -26,26 +26,26 @@ contract('SafeMathUInt', () => {
     describe('when then number is more than MAX_INT256', () => {
       it('should fail', async function () {
         expect(
-          await chain.isEthException(safeMathUInt.toInt256Safe(MAX_INT256.plus(1)))
+          await chain.isEthException(UInt256Lib.toInt256Safe(MAX_INT256.plus(1)))
         ).to.be.true;
       });
     });
 
     describe('when then number is MAX_INT256', () => {
       it('converts int to uint256 safely', async function () {
-        (await returnVal(safeMathUInt.toInt256Safe(MAX_INT256))).should.be.bignumber.eq(MAX_INT256);
+        (await returnVal(UInt256Lib.toInt256Safe(MAX_INT256))).should.be.bignumber.eq(MAX_INT256);
       });
     });
 
     describe('when then number is less than MAX_INT256', () => {
       it('converts int to uint256 safely', async function () {
-        (await returnVal(safeMathUInt.toInt256Safe(MAX_INT256.minus(1)))).should.be.bignumber.eq(MAX_INT256.minus(1));
+        (await returnVal(UInt256Lib.toInt256Safe(MAX_INT256.minus(1)))).should.be.bignumber.eq(MAX_INT256.minus(1));
       });
     });
 
     describe('when then number is 0', () => {
       it('converts int to uint256 safely', async function () {
-        (await returnVal(safeMathUInt.toInt256Safe(0))).should.be.bignumber.eq(0);
+        (await returnVal(UInt256Lib.toInt256Safe(0))).should.be.bignumber.eq(0);
       });
     });
   });
