99b146e11605d60461c5a792104cee13f5680f38
Nithin <nithin@fragments.org>
2019-03-05 14:02:18 -0800

    using a moving target rate

--- contracts/mocks/Mock.sol
+++ contracts/mocks/Mock.sol
@@ -2,7 +2,7 @@ pragma solidity 0.4.24;
 
 
 contract Mock {
-    event FunctionCalled(string functionName, address caller);
+    event FunctionCalled(string instanceName, string functionName, address caller);
     event FunctionArguments(uint256[] uintVals, int256[] intVals);
     event ReturnValueInt256(int256 val);
     event ReturnValueUInt256(uint256 val);
