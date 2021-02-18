7bd815f87a3264aed906ec48f24dcc689adb5ba4
Nithin Krishna <nithin@fragments.org>
2021-02-18 16:58:32 -0500

    verification script (#201)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,27 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import '@openzeppelin/hardhat-upgrades'
+import "@nomiclabs/hardhat-etherscan";
 import 'solidity-coverage'
 import 'hardhat-gas-reporter'
 
 require('./scripts/deploy')
 
 export default {
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_SECRET}`
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_SECRET}`
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_SECRET}`
+    }
+  },
   solidity: {
     compilers: [
       {
